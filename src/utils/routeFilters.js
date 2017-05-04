fd84b53bfb57767e569264d5655f9cf03c5ec5f2
Naveed Ahmad <naveedahmada036@gmail.com>
2017-05-04 23:43:57 +0500

    recirect 1/001-002 to 1/1-2

--- src/utils/routeFilters.js
+++ src/utils/routeFilters.js
@@ -1,4 +1,7 @@
 export default function isValidSurah(nextState, replaceState) {
+  if(__CLIENT__)
+    window.s=nextState;
+
   const chapterId = parseInt(nextState.params.chapterId, 10);
 
   if (isNaN(chapterId) || chapterId > 114 || chapterId < 1) {
@@ -7,12 +10,15 @@ export default function isValidSurah(nextState, replaceState) {
 
   if (nextState.params.range) {
     if (nextState.params.range.includes('-')) {
+      const [_from, _to] = nextState.params.range.split('-').map(num => num);
       const [from, to] = nextState.params.range.split('-').map(num => parseInt(num, 10));
 
       if (from > to) {
         replaceState(`/${chapterId}/${to}-${from}`);
       } else if (from === to) {
         replaceState(`/${chapterId}/${from}`);
+      } else if(_from.length != from.toString().length || _to.length != to.toString().length) {
+        replaceState(`/${chapterId}/${from}-${to}`);
       }
       // TODO: Add check to make sure the range is within the ayah limit
     }
