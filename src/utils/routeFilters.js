86f3ad96a063da3862dc671128350aadfb1f0702
Naveed Ahmad <naveedahmada036@gmail.com>
2017-05-05 00:49:23 +0500

    fixed routs

--- src/utils/routeFilters.js
+++ src/utils/routeFilters.js
@@ -1,9 +1,6 @@
 export default function isValidSurah(nextState, replaceState) {
-  if(__CLIENT__)
-    window.s=nextState;
-
   const chapterId = parseInt(nextState.params.chapterId, 10);
-
+  if (__CLIENT__) window.s = nextState;
   if (isNaN(chapterId) || chapterId > 114 || chapterId < 1) {
     replaceState('/error/invalid-surah');
   }
@@ -11,16 +8,44 @@ export default function isValidSurah(nextState, replaceState) {
   if (nextState.params.range) {
     if (nextState.params.range.includes('-')) {
       const [_from, _to] = nextState.params.range.split('-').map(num => num);
-      const [from, to] = nextState.params.range.split('-').map(num => parseInt(num, 10));
+      const [from, to] = nextState.params.range
+        .split('-')
+        .map(num => parseInt(num, 10));
 
       if (from > to) {
-        replaceState(`/${chapterId}/${to}-${from}`);
+        replaceState(`/${chapterId}/${to}-${from}${nextState.location.search}`);
       } else if (from === to) {
         replaceState(`/${chapterId}/${from}`);
-      } else if(_from.length != from.toString().length || _to.length != to.toString().length) {
-        replaceState(`/${chapterId}/${from}-${to}`);
+      } else if (
+        _from.length != from.toString().length ||
+        _to.length != to.toString().length
+      ) {
+        replaceState(`/${chapterId}/${from}-${to}${nextState.location.search}`);
       }
       // TODO: Add check to make sure the range is within the ayah limit
+    } else {
+      const verseId = parseInt(nextState.params.range, 10);
+
+      if (replaceChapterOrRange(nextState.params)) {
+        let location = `${nextState.location.pathname}${nextState.location.search}`;
+        location = location.replace(
+          /\/([0-9]+)\/([0-9]+)/,
+          `/${chapterId}/${verseId}`
+        );
+        replaceState(location);
+      }
     }
+  } else if (replaceChapterOrRange(nextState.params)) {
+    let location = `${nextState.location.pathname}${nextState.location.search}`;
+    location = location.replace(/\/([0-9]+)/, `/${chapterId}`);
+    replaceState(location);
   }
+}
+
+function replaceChapterOrRange(params) {
+  return (
+    params.chapterId.length !=
+      parseInt(params.chapterId, 10).toString().length ||
+    params.chapterId.length != parseInt(params.chapterId, 10).toString().length
+  );
 }
